6b00241732332f9c3af9e06e74e72f7f752ce779
Waqar Jamil <waqarkhokhar570@gmail.com>
2023-02-14 20:15:58 +0500

    Sending Welcome Email through Action Mailer

--- app/mailers/a_mailer.rb
+++ app/mailers/a_mailer.rb
@@ -1,8 +1,11 @@
 class AMailer < ApplicationMailer
     default :from => 'waqarkhokhar570@gmail.com'
+
     def send_signup_email(author)
         @author = author
-        mail(to:  @author.name,
-        subject:  'Thanks for creating Author for our amazing app' )
+        mail(
+        to:  (@author.name),
+        subject:  'Thanks for creating Author for our amazing app' 
+        )
     end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'waqarkhokhar570@gmail.com'
+  
   layout 'mailer'
 end
